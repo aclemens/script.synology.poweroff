53122afa7c89af948db33fdcb0d86a80b6e0e687
Andi Clemens <andi.clemens@googlemail.com>
2013-11-05 16:15:40 +0100

    use the paramiko SSH library for shutting down the Synology DiskStation

--- default.py
+++ default.py
@@ -1,31 +1,58 @@
+import xbmc, xbmcgui, xbmcaddon, sys
 
-import xbmc, xbmcgui, xbmcaddon
-import subprocess 
 
-def main(isAutostart=False):
-	
-	print 'script.synology.poweroff: Starting Synology DiskStation Power Off script'
-	
-	####### Read Settings
-	settings = xbmcaddon.Addon( id="script.synology.poweroff" )
-	language  = settings.getLocalizedString
-	
-	# basic settings
-	hostOrIp = settings.getSetting("hostOrIp")
-	username = settings.getSetting("username")
-	password = settings.getSetting("password")
-	poweroff_cmd = settings.getSetting("poweroff_cmd")
+def checkLibrary(module_name):
+    try:
+        __import__(module_name)
+        print "\tLibrary '%s' successfully loaded!" % module_name
+        return True
+    except ImportError:
+        print "\tError: Unable to load the library '%s'!" % module_name
+        dialog = xbmcgui.Dialog()
+        dialog.ok("Could not find a required library", "Make sure that the python library '%s' is installed on your system." % module_name)
+        exit()
+        sys.exit(1)
 
-	command = "sshpass -p \"%s\" ssh -o StrictHostKeyChecking=no %s@%s %s" % (
-		password, username, hostOrIp, poweroff_cmd)
 
-	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	output,stderr = process.communicate()
-	status = process.poll()
-	print output
+def start():
+    print 'script.synology.poweroff: Starting Synology DiskStation Power Off script'
 
-	print 'script.synology.poweroff: Closing Synology DiskStation Power Off script'
-	return
-	
+
+def exit():
+    print 'script.synology.poweroff: Closing Synology DiskStation Power Off script'
+
+
+def log(stream):
+    for line in stream:
+        print "\t%s" % line
+
+
+def main(isAutostart=False):    
+    start()
+
+    if not checkLibrary('paramiko'): import_failed()
+    import paramiko
+    
+    ####### Read Settings
+    settings = xbmcaddon.Addon( id="script.synology.poweroff" )
+    language  = settings.getLocalizedString
+    
+    # basic settings
+    hostOrIp = settings.getSetting("hostOrIp")
+    username = settings.getSetting("username")
+    password = settings.getSetting("password")
+    poweroff_cmd = settings.getSetting("poweroff_cmd")
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostOrIp, username=username, password=password)
+    stdin, stdout, stderr = ssh.exec_command(poweroff_cmd)
+    log(stdout)
+    log(stderr)
+    ssh.close()
+
+    exit()
+    
+    
 if __name__ == '__main__':
-	main()
+    main()
